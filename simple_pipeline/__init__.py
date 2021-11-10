d676014c0b33ff920a98a0dea82fd353f04cec6f
Lucas Fobian <50679363+lucasfbn@users.noreply.github.com>
2021-11-10 13:42:20 +0000

    Update __init__.py

--- simple_pipeline/__init__.py
+++ simple_pipeline/__init__.py
@@ -1,4 +1,4 @@
-from task import task, filter_task, set_pipeline, get_pipeline, delete_pipeline
-from map.parallel import par_map, par_map_unpack
-from map.sequential import seq_map, seq_map_unpack
-from pipeline import Pipeline
+from simple_pipeline.task import task, filter_task, set_pipeline, get_pipeline, delete_pipeline
+from simple_pipeline.map.parallel import par_map, par_map_unpack
+from simple_pipeline.map.sequential import seq_map, seq_map_unpack
+from simple_pipeline.pipeline import Pipeline
